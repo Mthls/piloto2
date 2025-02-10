ca17a4b3fc13e9891e2cb8b25cdc4416dbaad0dc
Matheus Lima <85462605+Mthls@users.noreply.github.com>
2025-02-10 17:51:38 -0300

    Add files via upload

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('contato/', views.contato, name='contato'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('ajuda/', views.ajuda, name='ajuda'),
+    path('local/', views.local, name='local'),
+]
---
+++ home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+def index(request):
+    return render(request, 'index.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def sobre(request):
+    return render(request, 'sobre.html')
+
+def ajuda(request):
+    return render(request, 'ajuda.html')
+
+def local(request):
+    return render(request, 'local.html')
+
+def perfil(request, usuario):
+    return render(request, 'perfil.html', {'usuario': usuario})
+
