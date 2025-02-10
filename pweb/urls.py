b14dbf8ce8357bb36c20150b22ede4075a2a9860
Matheus Lima <85462605+Mthls@users.noreply.github.com>
2025-02-10 17:57:33 -0300

    Update urls.py

--- pweb/urls.py
+++ pweb/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('exibiritem/<int:id>', views.exibiritem,name='exibiritem'),
 ]
